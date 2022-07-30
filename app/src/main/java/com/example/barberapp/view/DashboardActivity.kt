07d45a6f3af733a6cb48967b0eb108ccca3e8904
JiheeSon <jihee.sonsational@gmail.com>
2022-07-30 17:39:31 -0500

    added dashboard & navigation drawer ui

--- app/src/main/java/com/example/barberapp/view/DashboardActivity.kt
+++ app/src/main/java/com/example/barberapp/view/DashboardActivity.kt
@@ -2,11 +2,48 @@ package com.example.barberapp.view
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.Menu
+import android.view.MenuItem
+import androidx.core.view.GravityCompat
 import com.example.barberapp.R
+import com.example.barberapp.databinding.ActivityDashboardBinding
 
 class DashboardActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityDashboardBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_dashboard)
+        binding = ActivityDashboardBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        initNavBar()
+    }
+
+    private fun initNavBar() {
+        setSupportActionBar(binding.toolbar)
+        //supportActionBar?.setDisplayHomeAsUpEnabled(true)
+        //supportActionBar?.setHomeAsUpIndicator(R.drawable.ic_baseline_menu_24)
+
+//        binding.toolbar.setNavigationOnClickListener {
+//            binding.layoutDashboard.openDrawer(GravityCompat.START)
+//        }
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        menuInflater.inflate(R.menu.menu_notification, menu)
+        return super.onCreateOptionsMenu(menu)
+    }
+
+
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        if (item.itemId == android.R.id.home) {
+            if (binding.layoutDashboard.isDrawerOpen(GravityCompat.START)) {
+                binding.layoutDashboard.closeDrawer(GravityCompat.START)
+            } else {
+                binding.layoutDashboard.openDrawer(GravityCompat.START)
+            }
+        }
+        return super.onOptionsItemSelected(item)
     }
 }
