61ef666437c4c801b77bfa2727a9a7ad12c053de
JiheeSon <jihee.sonsational@gmail.com>
2022-08-01 02:26:13 -0500

    added barber list ui

---
+++ app/src/main/java/com/example/barberapp/view/appointment/BarberListActivity.kt
@@ -0,0 +1,12 @@
+package com.example.barberapp.view.appointment
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.barberapp.R
+
+class BarberListActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_barber_list)
+    }
+}
